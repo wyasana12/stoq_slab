d06f6a21ce35af56ab3ac5a3fe52261b610eb8f3
Rajwaazz <muhammadrajwa124@gmail.com>
2026-03-10 16:06:41 +0700

    [update] routes admin, staff

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RestockController;
+use App\Http\Controllers\DistributionController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/warehouses')->group(function () {
@@ -11,3 +12,9 @@ Route::prefix('/warehouses')->group(function () {
     Route::delete('restocks/{restock}', [RestockController::class, 'destroy']);
     Route::post('restocks/{restock}/confirm', [RestockController::class, 'confirm']);
 });
+
+Route::get('/distributions', [DistributionController::class, 'index']);
+Route::get('/distributions/{distribution}', [DistributionController::class, 'show']);
+Route::post('/distributions', [DistributionController::class, 'store']);
+Route::put('/distributions/{distribution}', [DistributionController::class, 'update']);
+Route::delete('/distributions/{distribution}', [DistributionController::class, 'destroy']);
