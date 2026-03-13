5ddd2cad5324c5db05b6166b667ec9bf5ab1eb29
wyasana12 <140244781+wyasana12@users.noreply.github.com>
2026-03-14 01:03:11 +0700

    Add: roleFeature;  permissionfeature; userFeature; Fix

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\StoreAndUpdateUser;
+use App\Http\Resources\User\UserDetailResource;
+use App\Http\Resources\User\UserListResource;
+use App\Models\User;
+use App\Services\UserService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function index(): JsonResponse
+    {
+        try {
+            $allUsers = $this->userService->getAllUsers(10);
+
+            return response()->json([
+                'success' => true,
+                'data' => UserListResource::collection($allUsers),
+            ], 200);
+        } catch (\Exception $err) {
+            return response()->json([
+                'success' => false,
+                'messages' => 'Failed to retrieve all users.',
+                'error' => $err->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function store(StoreAndUpdateUser $request): JsonResponse
+    {
+        try {
+            $createUser = $this->userService->createUser($request->validated());
+
+            return response()->json([
+                'success' => true,
+                'messages' => 'User created successful.',
+                'data' => new UserDetailResource($createUser),
+            ]);
+        } catch (\Exception $err) {
+            return response()->json([
+                'success' => false,
+                'messages' => 'Failed to create user.',
+                'error' => $err->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function show(User $user): JsonResponse {
+        try {
+            $userDetail = $this->userService->getUserDetail($user);
+
+            return response()->json([
+                'success' => true,
+                'data' => new UserDetailResource($userDetail),
+            ], 200);
+        } catch (\Exception $err) {
+            return response()->json([
+                'success' => true,
+                'messages' => 'Failed to retrieve user details.',
+                'error' => $err->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function update(StoreAndUpdateUser $request, User $user): JsonResponse
+    {
+        try {
+            $updateUser = $this->userService->updateUser($user, $request->validated());
+
+            return response()->json([
+                'success' => true,
+                'messages' => 'User updated successful.',
+                'data' => new UserDetailResource($updateUser),
+            ]);
+        } catch (\Exception $err) {
+            return response()->json([
+                'success' => false,
+                'messages' => 'Failed to updated user.',
+                'error' => $err->getMessage(),
+            ], 500);
+        }
+    }
+}
--- routes/superadmin.php
+++ routes/superadmin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SupplierController;
@@ -8,6 +9,11 @@ use App\Http\Controllers\WarehouseController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/superadmin')->name('superadmin.')->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('user.index');
+    Route::post('/users/create', [UserController::class, 'store'])->name('user.create');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('user.show');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('user.update');
+
     Route::get('/products', [ProductController::class, 'index'])->name('product.index');
     Route::post('/products/create', [ProductController::class, 'store'])->name('product.create');
     Route::get('/products/{product}', [ProductController::class, 'show'])->name('product.show');
